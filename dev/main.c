0a8a8b886a0f9be0a5d8eed32ef34a2776539363
StevePro7 <steveprozune@hotmail.com>
2020-06-13 21:19:15 +0100

    no message

--- dev/main.c
+++ dev/main.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
+void myJoyHandler( u16 joy, u16 changed, u16 state )
+{
+	if( joy != JOY_1 )
+	{
+		return;
+	}
+
+	if( state & changed & BUTTON_A )
+	{
+	}
+}
+
 void main()
 {
+	u8 num = 9;
+	u8 test = z80_vgm[ 2 ];
+
+	VRAM_releaseRegion( NULL );
+	XGM_resumePlay();
+	YM2612_enableDAC();
+	Z80_init();
+
+	JOY_init();
+	JOY_setEventHandler( myJoyHandler );
 	return;
 }
--- dev/main.h
+++ dev/main.h
@@ -1,6 +1,15 @@
 #ifndef __MAIN__
 #define __MAIN__
 
+#include "..\lib\joy.h"
+#include "..\lib\types.h"
+#include "..\lib\vram.h"
+#include "..\lib\xgm.h"
+#include "..\lib\ym2612.h"
+#include "..\lib\z80_ctrl.h"
+#include "..\lib\z80_mvsc.h"
+#include "..\lib\z80_vgm.h"
+
 //#ifdef _CONSOLE
 //#include "genesis.h"
 //#else
