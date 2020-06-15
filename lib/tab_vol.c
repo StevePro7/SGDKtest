ce8aba66195f982d518f59411cc5bb0bfbfcb429
StevePro7 <steveprozune@hotmail.com>
2020-06-15 20:10:32 +0100

    no message

--- lib/tab_vol.c
+++
@@ -1,4 +0,0 @@
-#include "tab_vol.h"
-
-// TODO add zeros
-const u8 tab_vol[ 0x1000 ] = { 0 };
--- lib/z80_vgm.c
+++
@@ -1,7 +0,0 @@
-#include "z80_vgm.h"
-
-// TODO fill with 228 x 16
-const u8 z80_vgm[ 3648 ] = 
-{ 
-	0
-};
